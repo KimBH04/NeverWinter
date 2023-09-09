d98681145c13e71964fbe8fc31ea4bb9fbcaa2f0
changwoomin <changwoomin426@gmail.com>
2023-09-09 19:19:11 +0900

    1. 에너미컨트롤 // 이동속도 회전속도 변수 추가 2. 에너미스폰포인트 배열 선언
    추후에 디테일하게 만들 예정

--- NeverWinter/Assets/1.Scripts/EnemyCtrl.cs
+++ NeverWinter/Assets/1.Scripts/EnemyCtrl.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class EnemyCtrl : MonoBehaviour
 {
+    public float move_Speed = 3.0f; // 이동 속도 
+    public float rotation_Speed = 120.0f; // 회전 속도 
+    
     private Transform target;
     private NavMeshAgent agent;
 
@@ -13,6 +16,9 @@ public class EnemyCtrl : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.Find("TARGET").transform;
 
+        agent.speed = move_Speed;
+        agent.angularSpeed = rotation_Speed;
+        
         agent.SetDestination(target.position);
     }
 }
--- NeverWinter/Assets/1.Scripts/EnemySpawnPoint.cs
+++ NeverWinter/Assets/1.Scripts/EnemySpawnPoint.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class EnemySpawnPoint : MonoBehaviour
 {
-    public GameObject enemy;
+    public GameObject[] enemy;
     public Transform spawnPoint;
 
     IEnumerator Start()
     {
         while (true)
         {
-            Instantiate(enemy, spawnPoint.position, Quaternion.Euler(Vector3.forward));
+            int randomIndex = Random.Range(0, enemy.Length);
+            GameObject selectEnemy = enemy[randomIndex];
+            
+            
+            Instantiate(selectEnemy, spawnPoint.position, Quaternion.Euler(Vector3.forward));
 
             yield return new WaitForSeconds(3);
         }
