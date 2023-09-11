8d07af38d1dc5d8d5d9b1cec5fc1fdf8e30a0ff7
changwoomin <changwoomin426@gmail.com>
2023-09-11 19:32:51 +0900

    카메라 컨트롤 1. 마우스휠 // 2. 키보드 으로 조작가능
    스피드는 나중에 변수 하나주고 변경 예정

--- NeverWinter/Assets/1.Scripts/CameraCtrl.cs
+++ NeverWinter/Assets/1.Scripts/CameraCtrl.cs
@@ -2,14 +2,40 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
-    public float wSpeed, hSpeed, zoomInOutSpeed;
+    public float  wSpeed, hSpeed, zoomInOutSpeed;
     private float X, Z, Zoom;
+    
 
     private float w = Screen.width / 25f;
     private float h = Screen.height / 10f;
 
+    private bool isPanning = false;
+    private Vector3 lastMousePosition;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(2))
+        {
+            isPanning = true;
+            lastMousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            isPanning = false;
+        }
+        
+        if (isPanning)
+        {
+            Vector3 deltaMouse = Input.mousePosition - lastMousePosition;
+            X = Mathf.Clamp(X - deltaMouse.x /50, -10, 10);
+            Z = Mathf.Clamp(Z - deltaMouse.y /50, -10, -2);
+           
+          
+            lastMousePosition = Input.mousePosition;
+        }
+        
+        
         if (Input.mousePosition.x > Screen.width - w)
         {
             X = Mathf.Clamp(X + wSpeed, -10, 10);
@@ -28,13 +54,13 @@ public class CameraCtrl : MonoBehaviour
             Z = Mathf.Clamp(Z - hSpeed, -10, -2);
         }
 
-        /*float x = Input.GetAxis("Horizontal") * speed;
-        float z = Input.GetAxis("Vertical") * speed;*/
+        float x = Input.GetAxisRaw("Horizontal")/5;
+        float z = Input.GetAxisRaw("Vertical")/5;
 
         float zoom = Input.GetAxis("Mouse ScrollWheel");
 
-        /*X = Mathf.Clamp(X + x, -10, 10);
-        Z = Mathf.Clamp(Z + z, -10, -2);*/
+        X = Mathf.Clamp(X + x, -10, 10);
+        Z = Mathf.Clamp(Z + z, -10, -2);
 
         Zoom = Mathf.Clamp(Zoom + zoom, -1, 0);
 
