c5981d4d598cd10b3b87c52c56a1852c3b8a489d
changwoomin <changwoomin426@gmail.com>
2023-09-12 21:25:44 +0900

    에너미 컨트롤 //.1.데미지를 받을 때 2.죽었을 때 // 추가

--- NeverWinter/Assets/1.Scripts/EnemyCtrl.cs
+++ NeverWinter/Assets/1.Scripts/EnemyCtrl.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 
 public class EnemyCtrl : MonoBehaviour
 {
-    // 체력
+    // 체력  #미완성#
     public float Enemy_HP;
    
     // 이동속도
@@ -18,6 +18,9 @@ public class EnemyCtrl : MonoBehaviour
     
     //능력 #미완성#
     public string Enemy_Spell;
+    
+    //적 사망여부 #미완성#
+    public bool isEnemyDie = false;
     
     private Transform target;
     private NavMeshAgent agent;
@@ -31,5 +34,40 @@ public class EnemyCtrl : MonoBehaviour
         agent.angularSpeed = Enemy_rotation_Speed;
         
         agent.SetDestination(target.position);
+    }
+    
+    // 적이 데미지 받았을 때 쓰는 함수
+    public void TakeDamage(float damage)
+    {
+        if (isEnemyDie)
+            return;
+
+        
+        Enemy_HP -= damage;
+
+        
+        if (Enemy_HP <= 0)
+        {
+            EnemyDie();
+        }
+    }
+
+    // 적이 죽었을 때 쓰는 함수  # 미완성 #
+    // 애니메이션 추가 예정
+    private void EnemyDie()
+    {
+        isEnemyDie = true;
+
+        
+
+        
+        agent.enabled = false;
+
+        
+        GetComponent<Collider>().enabled = false;
+
+        
+        Destroy(gameObject, 2.0f);
+
     }
 }
