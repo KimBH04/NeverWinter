319cf30eb2afe52ca6807b17e2130a739649c596
KimBH04 <kimbh0437@gmail.com>
2023-09-21 21:28:46 +0900

    돌산 모델 추가, 게임 매니저 싱글톤

--- NeverWinter/Assets/1.Scripts/GameManager.cs
+++ NeverWinter/Assets/1.Scripts/GameManager.cs
@@ -3,4 +3,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
 }
