a2221da138e64ae1f342692a0c87cf516e4073d5
changwoomin <changwoomin426@gmail.com>
2023-09-26 14:52:38 +0900

    배속 UI

---
+++ NeverWinter/Assets/1.Scripts/TimeUI/Time_UI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_UI : MonoBehaviour
+{
+    public GameObject Attach_1;
+    public GameObject Attach_1_5;
+    public GameObject Attach_2;
+    
+    public void attach_1()
+    {
+        Time.timeScale = 1.5f;
+        Attach_1.gameObject.SetActive(false);
+        Attach_1_5.gameObject.SetActive(true);
+        Debug.Log("1.5배");
+    }
+    public void attach_1_5()
+    {
+        Time.timeScale = 2f;
+        Attach_1_5.gameObject.SetActive(false);
+        Attach_2.gameObject.SetActive(true);
+        Debug.Log("2배");
+    }
+    public void attach_2()
+    {
+        Time.timeScale = 1f;
+        Attach_2.gameObject.SetActive(false);
+        Attach_1.gameObject.SetActive(true);
+        Debug.Log("1배");
+    }
+
+
+
+}
