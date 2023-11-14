7c5852c10620cc785e656b4cd18ad97a4f08382d
dlckddbs <booms5@naver.com>
2023-11-14 16:09:05 +0900

    크래시 결

--- NeverWinter/Assets/1.Scripts/BarricadeSummon.cs
+++ NeverWinter/Assets/1.Scripts/BarricadeSummon.cs
@@ -9,36 +9,43 @@ public class BarricadeSummon : MonoBehaviour
     [SerializeField]
     private GameObject barricadePrefab;
 
+    private bool summon;
 
-   private void Update()
-   {
-      if(summon&& Input.GetMouseButtonDown(0))
-      {
-         SummonBarricade();
-            Cost.Coin -= 40;
-      }
-   }
+
+    private void Update()
+    {
+        if (summon)
+        {
+            barricadeVisual.position = CameraCtrl.FloorPos + Camera.main.transform.position;
+            if (Input.GetMouseButtonDown(0))
+            {
+                SummonBarricade();
+                Cost.Coin -= 40;
+            }
+        }
+    }
 
     private Vector3 spawnPos;
 
-      if (Physics.Raycast(ray, out hit, 100f, 1 << 10))
-      {
-         summon = false;
-         SkillControl. instance.HideSkiiSetting(1);
-         spawnPos = hit.point;
-         int random = Random.Range(0, 2);
-         float y = (random==0)?45f:135f;
-         GameObject barricade = Instantiate(barricadePrefab, spawnPos, Quaternion.Euler(0,y,0));
-      }
-      
-      
-      
-   }
-   
-   public void click()
-   {
+    private void SummonBarricade()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, 1 << 10))
+        {
+            summon = false;
+            SkillControl.instance.HideSkiiSetting(1);
+            spawnPos = hit.point;
+            int random = Random.Range(0, 2);
+            float y = (random == 0) ? 45f : 135f;
+            GameObject barricade = Instantiate(barricadePrefab, spawnPos, Quaternion.Euler(0, y, 0));
+        }
+
+    }
+
+    public void click()
+    {
         AudioManager.instance.PlaySfx(AudioManager.Sfx.Ck);
         summon = true;
-   }
-   
+    }
+
 }
